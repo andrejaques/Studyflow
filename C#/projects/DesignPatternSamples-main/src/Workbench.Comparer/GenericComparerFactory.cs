968f36f4d04d9010bf3e8e6245b4ca93135023fc
andrejaques <andrejaques.b@gmail.com>
2020-12-28 19:24:56 -0300

    deisgn patterns course DIO

--- C#/projects/DesignPatternSamples-main/src/Workbench.Comparer/GenericComparerFactory.cs
+++ C#/projects/DesignPatternSamples-main/src/Workbench.Comparer/GenericComparerFactory.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿//Aplicando factory com create, utilizando strategy e inversão de controle
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Workbench.Comparer
 {
-    public class GenericComparerFactory<TEntity> : IEqualityComparer<TEntity>
+    public class GenericComparerFactory<TEntity> : IEqualityComparer<TEntity> // Classe com implementação da interface.
     {
         private Func<TEntity, object> Predicate { get; set; }
 
         private GenericComparerFactory() { }
 
-        public static GenericComparerFactory<TEntity> Create(Func<TEntity, object> predicate)
+        public static GenericComparerFactory<TEntity> Create(Func<TEntity, object> predicate) // Criação do strategy - factory com create passando uma arrow function
         {
             return new GenericComparerFactory<TEntity>() { Predicate = predicate };
         }
--- C#/projects/DesignPatternSamples-main/src/Workbench.Linq.Extensions.Tests/DistinctExtensionsTests.cs
+++ C#/projects/DesignPatternSamples-main/src/Workbench.Linq.Extensions.Tests/DistinctExtensionsTests.cs
@@ -16,15 +16,18 @@ namespace Workbench.Linq.Extensions.Tests
         [Fact(DisplayName = "Data uma coleção com 3 objetos sendo 2 iguais então o DISTINCT com uma comparação simples deve retornar uma lista com 2 objetos.")]
         public void ListagemComItensRepetidosComparecaoSimples()
         {
+            // Arrange
             IEnumerable<PessoaFisica> pessoas = new List<PessoaFisica>()
             {
-                new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "111.111.111-11" },
-                new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Paula", CPF = "111.111.111-11" },
-                new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "222.222.222-22" }
+                new PessoaFisica() { Nome = "Andre Jaques", NomeMae = "Ana", CPF = "111.111.111-11" },
+                new PessoaFisica() { Nome = "Andre Jaques", NomeMae = "Paula", CPF = "111.111.111-11" },
+                new PessoaFisica() { Nome = "Andre Jaques", NomeMae = "Ana", CPF = "222.222.222-22" }
             };
 
+            // Act
             IEnumerable<PessoaFisica> pessoasDiferentes = pessoas.Distinct(p => p.CPF);
 
+            // Assert
             Assert.NotNull(pessoasDiferentes);
             Assert.True(pessoasDiferentes.Any());
             Assert.Equal(2, pessoasDiferentes.Count());
@@ -36,9 +39,9 @@ namespace Workbench.Linq.Extensions.Tests
         {
             IEnumerable<PessoaFisica> pessoas = new List<PessoaFisica>()
             {
-                new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "111.111.111-11" },
-                new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Paula", CPF = "111.111.111-11" },
-                new PessoaFisica() { Nome = "Victor Fructuoso", NomeMae = "Ana", CPF = "222.222.222-22" }
+                new PessoaFisica() { Nome = "Andre Jaques", NomeMae = "Ana", CPF = "111.111.111-11" },
+                new PessoaFisica() { Nome = "Andre Jaques", NomeMae = "Paula", CPF = "111.111.111-11" },
+                new PessoaFisica() { Nome = "Andre Jaques", NomeMae = "Ana", CPF = "222.222.222-22" }
             };
 
             IEnumerable<PessoaFisica> pessoasDiferentes = pessoas.Distinct(p => new { p.Nome, p.NomeMae });
