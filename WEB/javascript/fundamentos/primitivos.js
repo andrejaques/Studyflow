c3e6efe579e1a47621c2f111c12ff6fb42742acb
André Jaques Batista <andrejaques.b@gmail.com>
2021-02-24 15:24:54 -0300

    Create primitivos.js
    adicionando o arquivo primitivos.js para ter um melhro começo no javascript

---
+++ WEB/javascript/fundamentos/primitivos.js
@@ -0,0 +1,7 @@
+/* Primeira aula - variáveis primitivas */
+
+let movie = "Avangers"; // String Literal
+let score = 10.89; // Number Literal
+let isValid = true; // boolean
+let name; // undefined
+let color = null; // redefinição
